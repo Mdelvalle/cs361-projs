a874203f3241aa2989d4087cac87b2f1b6946c9d
mdelvalle <mdelvalle@utexas.edu>
2013-10-02 20:41:37 -0700

    Stores nums in file and computes probabilities

--- proj3/Encoder.java
+++ proj3/Encoder.java
@@ -2,19 +2,37 @@ import java.util.*;
 import java.io.*;
 
 class Encoder {
-  public static void main(String[] args) { throws IOException
-    Integer n = Integer.valueOf(args[1]);
-    String[] lst = new String[n];
-    // String f = new String(args[0]);
-    try {
-      BufferedReader br = new BufferedReader(new FileReader(args[0]));
-    }
+  public static void main(String[] args) throws IOException {
+    // Globals
+    int n, sum;
+    int[] nums;
+    double entropy;
+    double[] probabilities;
+    String fileName;
+    char[] alphabet = new char[25];
+    // Map<String, int> alph_probs;
 
-    // Interpret list of n non-negative integers
+    // Initialization
+    fileName = args[0];
+    BufferedReader br = new BufferedReader
+                            (new FileReader(fileName));
+
+    // Interpret/Store list of n non-negative integers
+    n = Integer.parseInt(args[1]);
+    nums = new int[n];
+    sum = 0;
     String line = null;
     for(int i = 0; (line = br.readLine()) != null; i++) {
-      lst[i] = line;
-      System.out.println(lst[i]);
+      nums[i] = Integer.parseInt(line);
+      sum += nums[i];
     }
+
+    // Compute probabilites
+    probabilities = new double[n];
+    for(int i = 0; i < probabilities.length; i++) {
+      probabilities[i] = (double) nums[i] / sum;
+      System.out.println("probs: " + probabilities[i]);
+    }
+    
   }
 }
