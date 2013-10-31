872cfffb43535621777488ca0c841d51d415a451
miguel del valle <delvalle@cs.utexas.edu>
2013-10-31 17:23:44 -0500

    added proj4

---
+++ proj4/AES.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+class AES {
+  public subBytes() {
+
+  }
+
+  public shiftRows() {
+
+  }
+
+  public mixColumns() {
+
+  }
+
+  public addRoundKey() {
+
+  }
+
+  public static void main(String[] args) {
+    // 128-bit input
+    char input[] = 0x00000000000000000000000000000000;
+
+    // 128-bit key
+    int cipherKey = 0x00000000000000000000000000000000;
+
+    // rounds / cycles
+    final int rounds = 10;
+
+/*    File keyFile;
+    FileReader fR;
+    BufferedReader bR;
+
+    FileWriter fW;
+    BufferedWriter bW;
+*/
+
+    
+  }
+}
